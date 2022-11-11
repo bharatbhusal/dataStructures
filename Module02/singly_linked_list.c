dd002b1eaba0d8559c2d3e72ec61f679b58e9a64
Bharat Bhusal <bharatbhusal78@gmail.com>
2022-11-11 18:50:59 +0530

    commenting module 2

--- Module02/singly_linked_list.c
+++ Module02/singly_linked_list.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 // user defined data type
-struct node
+typedef struct node
 {
     int data;
     struct node *next;
-};
+} Node;
 
 // function to display the data of each node
-void display(struct node **head)
+void display(Node **head)
 {
-    struct node *temp = *head;
+    printf("Content of linked list\n");
+    Node *temp = *head;
     while (temp->next != NULL)
     {
         printf("%d ", temp->data);
@@ -20,102 +21,37 @@ void display(struct node **head)
     printf("%d\n", temp->data);
 }
 
-// function to return a pointer to a memory address of above data type allocated dynamically
-struct node *createNode(int val)
+int countNode(Node **head)
 {
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = val;
-    temp->next = NULL;
-    return temp;
-}
-
-// function to add a node in the begining of the linked list
-void addStart(struct node **head, int val)
-{
-    if (*head == NULL)
-        *head = createNode(val);
-    else
+    int count = 0;
+    Node *temp = *head;
+    while (temp != NULL)
     {
-        struct node *temp = createNode(val);
-        temp->next = *head;
-        *head = temp;
-    }
-}
-
-// function to add a node at the end of the linked list
-void addEnd(struct node **head, int val)
-{
-    if (*head == NULL)
-        *head = createNode(val);
-    else
-    {
-        struct node *temp = createNode(val);
-        struct node *runner = *head;
-        while (runner->next != NULL)
-            runner = runner->next;
-        runner->next = temp;
-    }
-}
-
-// function to insert a node before the given value
-void addBet(struct node **head, int val, int place)
-{
-    if ((*head)->data == place)
-        addStart(head, val);
-    else
-    {
-        struct node *temp = createNode(val);
-        struct node *runner = *head;
-        while (runner->next->data != place)
-            runner = runner->next;
-        temp->next = runner->next;
-        runner->next = temp;
-    }
-}
-
-// deleting first node of the linked list.
-void delStart(struct node **head)
-{
-    struct node *temp = *head;
-    *head = (*head)->next;
-    temp->next = NULL;
-    free(temp);
-}
-
-// deleting last node of the linked list.
-void delEnd(struct node **head)
-{
-    struct node *temp = *head;
-    struct node *dump = NULL;
-    while (temp->next->next != NULL)
+        count++;
         temp = temp->next;
-    dump = temp->next;
-    temp->next = NULL;
-    free(dump);
+    }
+    return count;
 }
 
-// deleting the node of the linked list which has value "place" stored.
-void delBet(struct node **head, int place)
+// displaying the nodes in reverse order
+void display_rev(Node **head)
 {
-    if ((*head)->data == place)
-        delStart(head);
-    else
-    {
-        struct node *runner = *head;
-        struct node *dump = NULL;
-        while (runner->next->data != place)
-            runner = runner->next;
-        dump = runner->next;
-        runner->next = runner->next->next;
-        dump->next = NULL;
-        free(dump);
-    }
+    printf("Content of linked list in reverse order\n");
+    Node *temp = *head;
+    int size = countNode(head);
+    int array[size];
+    for (int i = 0; temp != NULL; temp = temp->next, i++)
+        array[i] = temp->data;
+
+    for (int i = size - 1; i >= 0; i--)
+        printf("%d ", array[i]);
+    printf("\n");
 }
 
 // returning node number where "val" is stored
-int position(struct node **head, int val)
+int position(Node **head, int val)
 {
-    struct node *temp = *head;
+    Node *temp = *head;
     int pos = 1;
 
     while (temp->data != val && temp->next != NULL)
@@ -130,8 +66,11 @@ int position(struct node **head, int val)
 }
 
 // searching for the "val"
-void search(struct node **head, int val)
+void search(Node **head)
 {
+    int val;
+    printf("Search element: ");
+    scanf("%d", &val);
     int pos = position(head, val);
     if (pos == -1)
         printf("%d is not in the list.\n", val);
@@ -139,48 +78,159 @@ void search(struct node **head, int val)
         printf("%d is in node %d.\n", val, pos);
 }
 
-// displaying the nodes in reverse order
-void display_rev(struct node **head)
+// function to return a pointer to a memory address of above data type allocated dynamically
+Node *createNode()
 {
-    struct node *temp = *head;
-    struct node *sup = NULL;
-    while (temp != sup && temp != NULL)
+    int val;
+    Node *temp = (Node *)malloc(sizeof(Node));
+    printf("Value in the node: ");
+    scanf("%d", &val);
+    temp->data = val;
+    temp->next = NULL;
+    return temp;
+}
+
+// function to add a node in the begining of the linked list
+void addStart(Node **head)
+{
+    printf("Adding a node in the begining of the list.\n");
+    Node *temp = createNode();
+    if (countNode(head) == 0)
+        *head = temp;
+    else
     {
-        struct node *temp1 = *head;
-        while (temp1->next != NULL && temp1->next != sup)
-        {
-            temp1 = temp1->next;
-        }
-        printf("%d ", temp1->data);
-        sup = temp1;
-        temp = temp->next;
+        temp->next = *head;
+        *head = temp;
     }
-    printf("\n");
+}
+
+// function to add a node at the end of the linked list
+void addEnd(Node **head)
+{
+    printf("Adding a node at the end of the list.\n");
+    Node *temp = createNode();
+    if (countNode(head) == 0)
+        *head = temp;
+    else
+    {
+        Node *runner = *head;
+        while (runner->next != NULL)
+            runner = runner->next;
+        runner->next = temp;
+    }
+}
+
+// function to insert a node before the given value
+void addBet(Node **head)
+{
+    printf("Adding a node in middle of the list.\n");
+    int place;
+    printf("Before which node to add a node: ");
+    scanf("%d", &place);
+    // printf("Adding a node before %d in the list.\n", place);
+    Node *runner = *head;
+    if (runner->data == place)
+        addStart(head);
+    else
+    {
+        Node *temp = createNode();
+        while (runner->next != NULL && runner->next->data != place)
+            runner = runner->next;
+        if (runner->next != NULL && runner->next->data == place)
+        {
+            temp->next = runner->next;
+            runner->next = temp;
+        }
+        else
+            printf("%d does not exist in the list. So, node is not added.\n", place);
+    }
+}
+
+// deleting first node of the linked list.
+void delStart(Node **head)
+{
+    printf("Deleting first node.\n");
+    if (countNode(head) == 0)
+        printf("List is empty!\n");
+    else
+    {
+        Node *dump = *head;
+        *head = (*head)->next;
+        dump->next = NULL;
+        free(dump);
+    }
+}
+
+// deleting last node of the linked list.
+void delEnd(Node **head)
+{
+    printf("Deleting last node.\n");
+    if (countNode(head) == 0)
+        printf("List is empty!\n");
+    else
+    {
+        Node *temp = *head;
+        Node *dump = NULL;
+        while (temp->next->next != NULL)
+            temp = temp->next;
+        dump = temp->next;
+        temp->next = NULL;
+        free(dump);
+    }
+}
+
+// deleting the node of the linked list which has value "place" stored.
+void delBet(Node **head)
+{
+    int place;
+    printf("Deleting middle node\n");
+    printf("To delete node(value): ");
+    scanf("%d", &place);
+    // printf("Deleting the node with value %d in the list.\n", place);
+    Node *runner = *head;
+    if (runner->data == place)
+        delStart(head);
+    else
+    {
+        Node *dump = NULL;
+        while (runner->next != NULL && runner->next->data != place)
+            runner = runner->next;
+        if (runner->next != NULL && runner->next->data == place)
+        {
+            dump = runner->next;
+            runner->next = runner->next->next;
+            dump->next = NULL;
+            free(dump);
+        }
+        else
+            printf("%d does not exist in the list.\n", place);
+    }
 }
 
 // driving function
 int main()
 {
     // head of linked list in stack with NULL value
-    struct node *head = NULL;
+    Node *head = NULL;
     // adding nodes in different positions as defined in the respective function above.
-    addStart(&head, 89);
-    addStart(&head, 1);
-    addEnd(&head, 0);
-    addEnd(&head, 150);
-    addBet(&head, 111, 89);
+    addStart(&head);
+    addStart(&head);
+    addEnd(&head);
+    addEnd(&head);
+    display(&head);
+    addBet(&head);
     // displaying the values of the linked list
     display(&head);
-    // searching for 0 in the list.
-    search(&head, 0);
-    // displaying the values of linked list in reverse order.
     display_rev(&head);
+    // searching for element in the list.
+    search(&head);
+    // displaying the values of linked list in reverse order.
     // deleting the nodes in different positions as defined in the respective function above.
     delStart(&head);
     display(&head);
     delEnd(&head);
     display(&head);
-    delBet(&head, 89);
+    delBet(&head);
     display(&head);
     return 0;
 }
--- Module02/tempCodeRunnerFile.c
+++ Module02/tempCodeRunnerFile.c
@@ -1,11 +1,1 @@
-// function to display the data in the doubly linked list,
-void display(struct node **head)
-{
-    struct node *temp = *head;
-    while (temp->next != NULL)
-    {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("%d\n", temp->data);
-}
+struct node
